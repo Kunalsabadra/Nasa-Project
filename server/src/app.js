9470c9d9f51073d05ddf8d7d7ee6dadbe9c445c7
kunalsabadra <kunalsabadra@gmail.com>
2024-11-06 11:49:14 +0530

    Added script for deployment of FE with BE

--- server/src/app.js
+++ server/src/app.js
@@ -1,12 +1,12 @@
+const path = require("path");
 const express = require("express");
 const app = express();
 const planetsRouter = require("./routes/planets/planets.routes");
 const cors = require("cors");
 
-app.use(cors({origin:
-    'http://localhost:3000'
-}));
+app.use(cors({ origin: "http://localhost:3000" }));
 app.use(express.json());
 app.use(planetsRouter);
+app.use(express.static(path.join(__dirname, "..", "public")));
 
 module.exports = app;
