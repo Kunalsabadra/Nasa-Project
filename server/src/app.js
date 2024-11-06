a99465e6e4f513d0fde5758c95023ebff10c5c01
kunalsabadra <kunalsabadra@gmail.com>
2024-11-06 12:03:18 +0530

    Added Logging of Requests using Morgan

--- server/src/app.js
+++ server/src/app.js
@@ -3,8 +3,10 @@ const express = require("express");
 const app = express();
 const planetsRouter = require("./routes/planets/planets.routes");
 const cors = require("cors");
+const morgan = require("morgan")
 
 app.use(cors({ origin: "http://localhost:3000" }));
+app.use(morgan('combined'))
 app.use(express.json());
 app.use(planetsRouter);
 app.use(express.static(path.join(__dirname, "..", "public")));
